459af16347c6848117d50c60362f37c8a9daf5b1
Craig Freeman <craigfreeman@gmail.com>
2015-11-06 21:10:48 -0700

    Minor style changes

--- wp-content/themes/germanschool/header.php
+++ wp-content/themes/germanschool/header.php
@@ -25,6 +25,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link href='//fonts.googleapis.com/css?family=Germania+One' rel='stylesheet' type='text/css'>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
